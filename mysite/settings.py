839e40c488524c6874a0baabb411258bbb3965ed
macalistervadim <macalistervadim@yandex.ru>
2023-08-14 13:10:29 +0600

    Change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'NAME': 'blog',
+ 'USER': 'postgres',
+ 'PASSWORD': 'admin',
+ }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
